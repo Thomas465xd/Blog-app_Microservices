d482fff288c3763674b737871a6afa161a947ceb
Thomas Schrödinger <160943727+Thomas465xd@users.noreply.github.com>
2025-07-02 16:40:26 -0400

    *Completed basic implementation of the posts module* - Added post route /posts to handle post creation (title and content) - Added get posts route /posts to retrieve all posts

--- posts/src/index.ts
+++ posts/src/index.ts
@@ -1,8 +1,17 @@
 import express from 'express';
-import { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import type { Request, Response } from 'express';
 import colors from 'colors';
+import morgan from 'morgan';
+import { randomBytes } from 'crypto'; 
 
 const app = express(); 
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+
+// Logs
+app.use(morgan("dev"));
 
 // Data object to simulate a database
 const posts = {
@@ -10,11 +19,21 @@ const posts = {
 }; 
 
 app.get("/posts", (req: Request, res: Response) => {
-
+    res.send(posts); 
 }); 
 
 app.post("/posts", (req: Request, res: Response) => {
+    const id = randomBytes(4).toString("hex"); 
 
+    const { title, content } = req.body; 
+
+    posts[id] = { 
+        id, 
+        title, 
+        content
+    }; 
+
+    res.status(201).send(posts[id]); 
 }); 
 
 app.listen(4000, () => {
